cdeed3edd579d3fe2598fcb78e0a7cfac8ed071f
Johan Calderón <johan.calderon@stratesys-ts.com>
2022-07-05 16:18:53 -0500

    added enviroment var to send mail

--- utils/mail.js
+++ utils/mail.js
@@ -1,11 +1,11 @@
 const nodemailer = require('nodemailer');
 
-const host=process.env.host_smtp || 'smtpout.secureserver.net';
-const service=process.env.service_smtp || 'goddady';
-const port=process.env.port_smtp || '465';
-const user=process.env.user_smtp || 'johan.calderon@biobayter.com';
-const pass=process.env.user_smtp || 'Johan80739565.';
-const to=process.env.user_smtp || 'nahhoj@hotmail.com';
+const host=process.env.host_smtp;
+const service=process.env.service_smtp;
+const port=process.env.port_smtp;
+const user=process.env.user_smtp;
+const pass=process.env.user_pass;
+const to=process.env.user_to;
 
 
 const transporter = nodemailer.createTransport({
